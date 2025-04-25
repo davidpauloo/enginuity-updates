dbb3c72646bb2116957f9bb932e33081e96ae809
Paulo David <pau@Paulos-MacBook-Air.local>
2025-04-25 13:36:29 +0800

    Chat feature completed

---
+++ frontend/chat-front-end/tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}", 
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ed3849', 
+      },
+      fontFamily: {
+        primary: ['Poppins', 'sans-serif'], 
+      },
+    },
+  },
+  plugins: [require('daisyui')], 
+  daisyui: {
+    themes: ["light","dark","acid",]
+    
+  },
+}
